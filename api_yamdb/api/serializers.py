06af78fcb4f46e3ee849d94bd4590303ce628764
Fedorov <fan8641@yandex.ru>
2022-11-15 22:09:19 +0300

    comment

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from reviews.models import Comment, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True,
+    )
+
+    class Meta:
+        fields = ("id", "text", "author", "score", "pub_date")
+        model = Review
+
+    def validate(self, data):
+        if self.context['request'].method == 'POST':
+            user = self.context['request'].user
+            title_id = self.context['view'].kwargs.get('title_id')
+            if Review.objects.filter(author=user, title_id=title_id).exists():
+                raise serializers.ValidationError('Ваш отзыв уже существует.')
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_date' )
+        model = Comment
