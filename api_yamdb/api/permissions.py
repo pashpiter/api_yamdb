913b21f796b8770a6ec65155f18bb579cd608651
Igor <igor.krupcko2010@yandex.ru>
2022-11-22 00:42:24 +0300

    change permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,11 +1,13 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+from reviews.models import ADMIN_ROLE, MODERATOR_ROLE
+
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
                 or (request.user.is_authenticated
-                    and (request.user.role == 'admin'
+                    and (request.user.role == ADMIN_ROLE
                          or request.user.is_superuser)))
 
 
@@ -16,16 +18,18 @@ class IsAuthorOrModeratorOrAdminOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
                 or request.user == obj.author
-                or request.user.role == 'moderator'
-                or request.user.role == 'admin'
+                or request.user.role == MODERATOR_ROLE
+                or request.user.role == ADMIN_ROLE
                 or request.user.is_superuser)
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return not request.user.is_anonymous and (request.user.role == 'admin'
-                                                  or request.user.is_superuser)
+        return not request.user.is_anonymous and (
+                request.user.role == ADMIN_ROLE
+                or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
-        return not request.user.is_anonymous and (request.user.role == 'admin'
-                                                  or request.user.is_superuser)
+        return not request.user.is_anonymous and (
+                request.user.role == ADMIN_ROLE
+                or request.user.is_superuser)
