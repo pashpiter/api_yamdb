680abcfca06dd6e073a168bd2faf0a66ba31a84f
Павел Дровнин <pavel98rus@gmail.com>
2022-11-22 02:04:21 +0300

    add EMAIL_HOST_USER

--- api_yamdb/users/utils.py
+++ api_yamdb/users/utils.py
@@ -2,6 +2,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from api_yamdb.settings import EMAIL_HOST_USER
+
 
 def create_confirmation_code(user):
     return default_token_generator.make_token(user)
@@ -14,7 +16,7 @@ def send_confirmation_code(user):
                  'регистрацию Вам необходимо отправить запрос на адрес '
                  f'api/v1/auth/token/ с username: {user.username} и с '
                  f'кодом: {user.confirmation_code}'),
-        from_email='admin.45cohort@yandex.ru',
+        from_email=EMAIL_HOST_USER,
         recipient_list=[user.email]
     )
 
